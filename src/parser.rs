c9dbcbecbbac5d37874fee5153cf5365fc0cb206
hitsan <soledewa2828@gmail.com>
2024-08-18 08:31:24 +0000

    implement line parser

--- src/parser.rs
+++ src/parser.rs
@@ -6,7 +6,8 @@ pub enum Md {
 
 #[derive(Debug, PartialEq)]
 pub enum Emphasis {
-    Text(String)
+    Text(String),
+    Italic(Box<Emphasis>)
 }
 
 #[derive(Debug, PartialEq)]
@@ -32,8 +33,18 @@ fn heading(sentence: &str) -> Option<ParsedResult<Md>> {
 }
 
 fn line(sentence: &str) -> Option<ParsedResult<Md>> {
-    text(sentence).map(|r| {
-        ParsedResult::new(Md::Line(r.token), &"")
+    let parsers = vec!(italic, text);
+    parsers.iter().find_map(|f| f(sentence).and_then(
+        |r| Some(ParsedResult::new(Md::Line(r.token), &""))
+    ))
+}
+
+fn italic(sentence: &str) -> Option<ParsedResult<Emphasis>> {
+    if !sentence.starts_with("*") { return None }
+    sentence[1..].find("*").and_then(|n| {
+        let s = text(&sentence[1..(n+1)]).unwrap();
+        let ret = ParsedResult::new(Emphasis::Italic(Box::new(s.token)), &sentence[(n+2)..]);
+        Some(ret)
     })
 }
 
@@ -56,8 +67,7 @@ mod tests {
     #[test]
     fn test_line() {
         let test_word = "Hello World!";
-        let md_ans = "Hello World!".to_string();
-        let md_ans = Emphasis::Text(md_ans);
+        let md_ans = Emphasis::Text("Hello World!".to_string());
         assert_eq!(parse(&test_word), ParsedResult{ token: Md::Line(md_ans), rest: &""});
     }
 
@@ -67,6 +77,14 @@ mod tests {
         let md_ans = "Hello World!".to_string();
         let ans = text(&test_word).unwrap();
         assert_eq!(ans, ParsedResult{ token: Emphasis::Text(md_ans), rest: &""});
+    }
+
+    #[test]
+    fn test_italic() {
+        let test_word = "*Hello World!*";
+        let md_ans = Box::new(Emphasis::Text("Hello World!".to_string()));
+        let ans = italic(&test_word).unwrap();
+        assert_eq!(ans, ParsedResult{ token: Emphasis::Italic(md_ans), rest: &""});
     }
 
     #[test]
