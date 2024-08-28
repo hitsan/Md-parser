313e2cc5d5184b0a90444aac522aa7908881c7c2
hitsan <soledewa2828@gmail.com>
2024-08-28 13:39:41 +0000

    add header parsing function

--- src/parser/table.rs
+++ src/parser/table.rs
@@ -1,16 +1,27 @@
 use crate::parser::parser::*;
+use super::line::terms;
 
-// fn repeate() -> {
-    
-// }
+fn header(sentence: &str) -> Option<TableHeader> {
+    if !sentence.starts_with("|") && !sentence.ends_with("|") {
+        return None
+    }
+    let len = sentence.len();
+    let sentence = &sentence[1..(len-1)];
+    let sentences = sentence.split("|");
+    let mut ret: Vec<Vec<Emphasis>> = vec!();
+    for s in sentences {
+        if s == "" { return None }
+        ret.push(terms(s));
+    }
+    Some(TableHeader(ret))
+}
 
 pub fn table(sentence: &str) -> Option<Md> {
-    let header = Emphasis::Text("Hello".to_string());
-    let header = TableHeader(vec!(vec!(header)));
+    let h = header(sentence)?;
 
     let data = Emphasis::Text("world".to_string());
     let data = TableData(vec!(vec!(vec!(data))));
-    Some(Md::Table(1, header, data))
+    Some(Md::Table(1, h, data))
 }
 
 #[cfg(test)]
