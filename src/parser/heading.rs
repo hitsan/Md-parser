7349be63c7d4e7ecef0a441817a98da35f16a6ca
hitsan <soledewa2828@gmail.com>
2024-08-30 14:46:24 +0000

    apply multiline heading

--- src/parser/heading.rs
+++ src/parser/heading.rs
@@ -1,14 +1,18 @@
 use crate::parser::parser::*;
 use super::line::words;
 
-pub fn heading(text: &str) -> Option<ParsedResult<Md>> {
+pub fn heading(texts: &str) -> Option<ParsedResult<Md>> {
     ["#", "##", "###"].iter().find_map(|p| {
+        let (text, rest) = if let Some(n) = texts.find("\n") {
+            (&texts[..n], &texts[(n+1)..])
+        } else {
+            (texts, "")
+        };
         let text = consume(text, p)?;
         let text = space(text)?;
         let tokens = words(&text);
         let token = Md::Heading(p.len(), tokens);
-        // Some(ret)
-        Some(ParsedResult::new(token,""))
+        Some(ParsedResult::new(token, rest))
     })
 }
 
@@ -37,6 +41,14 @@ mod tests {
         let token = vec!(Word::Normal("Hello World!".to_string()));
         let token = Md::Heading(3, token);
         assert_eq!(heading(&test_word), Some(ParsedResult{token, rest: ""}));
+    }
+
+    #[test]
+    fn test_heading_multiline() {
+        let test_word = "# Hello \nWorld!";
+        let token = vec!(Word::Normal("Hello ".to_string()));
+        let token = Md::Heading(1, token);
+        assert_eq!(heading(&test_word), Some(ParsedResult{token, rest: "World!"}));
     }
 
 }
