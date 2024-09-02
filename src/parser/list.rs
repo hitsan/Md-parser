44993a2e7ea5fe958125810e897955efebd0ff70
hitsan <soledewa2828@gmail.com>
2024-09-02 10:57:12 +0000

    Add list file

---
+++ src/parser/list.rs
@@ -0,0 +1,15 @@
+use crate::parser::parser::*;
+use super::sentence::words;
+
+pub fn list(texts: &str) {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list() {
+
+    }
+}
