4e6a228f911e62f8f9d6c68937ad134b7e1aec83
hitsan <soledewa2828@gmail.com>
2024-09-04 13:26:14 +0000

    Change tab_count spec

--- src/parser/list.rs
+++ src/parser/list.rs
@@ -7,14 +7,14 @@ fn count_tab(mut texts: &str, tab_num: usize) -> Option<ParsedResult<usize>> {
         texts = text;
         num += 1;
     }
-    if num < tab_num {
+    if num <= tab_num {
         None
     } else {
         Some(ParsedResult{token: num, rest: texts})
     }
 }
 
-fn item(texts: &str) -> Option<ParsedResult<Item>> {
+fn item(texts: &str, tab_num: usize) -> Option<ParsedResult<Item>> {
     let (text, rest) = if let Some(n) = texts.find("\n") {
         (&texts[..n], &texts[(n+1)..])
     } else {
@@ -29,7 +29,7 @@ fn item(texts: &str) -> Option<ParsedResult<Item>> {
 
 fn items(mut texts: &str) -> Option<ParsedResult<Items>> {
     let mut items: Vec<Item> = vec!();
-    while let Some(i) = item(texts) {
+    while let Some(i) = item(texts, 0) {
         items.push(i.token);
         texts = i.rest;
     }
@@ -49,20 +49,20 @@ mod tests {
         let w = Words(vec!(n));
         let l = Item(w, None);
         let rest = "";
-        assert_eq!(item(&test_word), Some(ParsedResult{token: l, rest}));
+        assert_eq!(item(&test_word, 0), Some(ParsedResult{token: l, rest}));
 
         let test_word = "- Hello World!";
         let n = Word::Normal("Hello World!".to_string());
         let w = Words(vec!(n));
         let l = Item(w, None);
         let rest = "";
-        assert_eq!(item(&test_word), Some(ParsedResult{token: l, rest}));
+        assert_eq!(item(&test_word, 0), Some(ParsedResult{token: l, rest}));
 
         let test_word = "Hello World!";
-        assert_eq!(item(&test_word), None);
+        assert_eq!(item(&test_word, 0), None);
 
         let test_word = "-Hello World!";
-        assert_eq!(item(&test_word), None);
+        assert_eq!(item(&test_word, 0), None);
     }
 
     #[test]
@@ -97,12 +97,12 @@ mod tests {
         assert_eq!(count_tab(text, 1), None);
 
         let text = "hello";
-        assert_eq!(count_tab(text, 0), Some(ParsedResult{token: 0, rest: "hello"}));
+        assert_eq!(count_tab(text, 0), None);
 
         let text = "  hello";
-        assert_eq!(count_tab(text, 1), Some(ParsedResult{token: 1, rest: "hello"}));
+        assert_eq!(count_tab(text, 1), None);
 
-        let text = "   hello";
-        assert_eq!(count_tab(text, 1), Some(ParsedResult{token: 1, rest: " hello"}));
+        let text = "     hello";
+        assert_eq!(count_tab(text, 1), Some(ParsedResult{token: 2, rest: " hello"}));
     }
 }
