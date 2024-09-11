cfe8028f281630f2d53805ff635325912f72fbdb
hitsan <soledewa2828@gmail.com>
2024-09-11 11:51:56 +0000

    implement a function that converts word to html.

--- src/convert/convert.rs
+++ src/convert/convert.rs
@@ -1,13 +1,16 @@
 use crate::parser::parser::{Md,Word,Words};
 
-fn convert_word<'a>(word: &'a Word) -> &'a String {
+fn convert_word<'a>(word: &'a Word) -> String {
     match word {
-        Word::Normal(val) => val,
-        _ => panic!("illegal word!")
+        Word::Normal(val) => val.clone(),
+        Word::Italic(words) => format!("<i>{}</i>", convert_words(&words)),
+        Word::Bold(words) => format!("<b>{}</b>", convert_words(&words)),
+        Word::StrikeThough(words) => format!("<s>{}</s>", convert_words(&words)),
+        Word::Underline(words) => format!("<u>{}</u>", convert_words(&words)),
     }
 }
 
-fn convert_words<'a>(words: &'a Words) -> &'a String {
+fn convert_words<'a>(words: &'a Words) -> String {
     match words.0.first() {
         Some(val) => convert_word(val),
         _ => panic!("illegal words!")
@@ -33,8 +36,25 @@ mod tests {
         assert_eq!(to_html(md), "<h1>Hello</h1>".to_string());
     }
 
+    #[test]
     fn test_word() {
         let word = normal_word!("Hello");
-        assert_eq!(convert_word(&word), &"Hello".to_string());
+        assert_eq!(convert_word(&word), "Hello".to_string());
+
+        let word = normal_word!("Hello");
+        let italic = Word::Italic(words!(word));
+        assert_eq!(convert_word(&italic), "<i>Hello</i>".to_string());
+
+        let word = normal_word!("Hello");
+        let bold = Word::Bold(words!(word));
+        assert_eq!(convert_word(&bold), "<b>Hello</b>".to_string());
+
+        let word = normal_word!("Hello");
+        let strike = Word::StrikeThough(words!(word));
+        assert_eq!(convert_word(&strike), "<s>Hello</s>".to_string());
+
+        let word = normal_word!("Hello");
+        let line = Word::Underline(words!(word));
+        assert_eq!(convert_word(&line), "<u>Hello</u>".to_string());
     }
 }
