b1068bec915b56ae4f23b5da2b3ef239381406cb
hitsan <soledewa2828@gmail.com>
2024-09-14 16:30:50 +0000

    implement output html file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,25 @@ mod parser;
 mod convert;
 mod emit;
 
-fn main() {
-    let hello = "# Hello world!";
-    let a = parser::parser::parse(&hello);
-    println!("{:?}", a)
+use std::fs;
+use std::io;
+
+fn write_to_file(path: &str, content: &str) -> io::Result<()> {
+    fs::write(path, content)?;
+    Ok(())
+}
+
+fn read_file_to_string(path: &str) -> io::Result<String> {
+    let contents = fs::read_to_string(path)?;
+    Ok(contents)
+}
+
+fn main()  -> io::Result<()> {
+    let contents = read_file_to_string("./test.md")?;
+    let contents: &str = &contents;
+    let mds = parser::parser::parse(&contents);
+    let output = convert::convert::mds_to_html(&mds);
+    let output: &str = &output;
+    write_to_file("./test.html", output)?;
+    Ok(())
 }
